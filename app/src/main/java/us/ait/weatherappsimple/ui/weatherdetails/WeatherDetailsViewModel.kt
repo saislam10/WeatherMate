1955fc53080da4a8e8180244212f711becdfe2a3
Safwan Islam <saislam@davidson.edu>
2023-05-15 16:57:13 +0200

    Push

--- app/src/main/java/us/ait/weatherappsimple/ui/weatherdetails/WeatherDetailsViewModel.kt
+++ app/src/main/java/us/ait/weatherappsimple/ui/weatherdetails/WeatherDetailsViewModel.kt
@@ -1,5 +1,7 @@
 package us.ait.weatherappsimple.ui.weatherdetails
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
@@ -7,6 +9,12 @@ import androidx.compose.runtime.State
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okio.IOException
 import us.ait.weatherappsimple.data.ForecastResponse.ForecastData
 import us.ait.weatherappsimple.data.RetrofitInstance
 import us.ait.weatherappsimple.data.WeatherApiService
@@ -20,6 +28,7 @@ class WeatherDetailsViewModel(private val apiService: WeatherApiService) : ViewM
     val weatherResult: State<WeatherResponse?> get() = _weatherResult
     val forecastResult = mutableStateOf<List<ForecastData>?>(null)
 
+    val weatherIcon: MutableStateFlow<Bitmap?> = MutableStateFlow(null)
 
 
     private val _errorMessage = MutableLiveData<String?>()
@@ -60,6 +69,30 @@ class WeatherDetailsViewModel(private val apiService: WeatherApiService) : ViewM
             }
         }
     }
+//    fun fetchWeatherIcon(icon: String) {
+//        viewModelScope.launch {
+//            val bitmap = fetchImageAsBitmap("https://openweathermap.org/img/w/$icon.png")
+//            weatherIcon.value = bitmap
+//        }
+//    }
+//
+//    private suspend fun fetchImageAsBitmap(url: String): Bitmap? {
+//        val request = Request.Builder()
+//            .url(url)
+//            .build()
+//
+//        return withContext(Dispatchers.IO) {
+//            OkHttpClient().newCall(request).execute().use { response ->
+//                if (!response.isSuccessful) throw IOException("Unexpected code $response")
+//
+//                val inputStream = response.body?.byteStream()
+//                val bitmap = BitmapFactory.decodeStream(inputStream)
+//                inputStream?.close()
+//
+//                return@withContext bitmap
+//            }
+//        }
+//    }
 
 
 
